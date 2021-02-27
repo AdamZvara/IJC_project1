cab970d32cc9de75b3eb67ba2d594e2f6ebfe2f9
Adam Zvara <xzvara01@stud.fit.vutbr.cz>
2021-02-27 22:23:01 +0100

    Initial version of the project.

---
+++ bitset.h
@@ -0,0 +1,9 @@
+/**
+ *@brief: Implementacia bitoveho pola, projekt 1 - IJC 
+ *@file: bitset.h
+ *@author: Adam Zvara, xzvara01 FIT
+ *@date: 27.2.2021
+ *Prelozene: gcc 9.3 
+ */
+
+
---
+++ makefile
@@ -0,0 +1,4 @@
+CC=gcc
+CFLAGS= -std=C11 -Wall -Wextra -pedantic
+
+
