379257e3134943c251ac0cf12b4402aaa3982045
Adam Zvara <xzvara01@stud.fit.vutbr.cz>
2021-03-01 21:24:10 +0100

    Added doxygen HTML documentation. Implemented Eratosthenes function.

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['bitset_2eh_0',['bitset.h',['../bitset_8h.html',1,'']]],
+  ['bitset_5falloc_1',['bitset_alloc',['../bitset_8h.html#a7c096bdf16b851a3e10b43c0acb8de60',1,'bitset.h']]],
+  ['bitset_5fcreate_2',['bitset_create',['../bitset_8h.html#aad4b5748beb7a8d9640862bdad74cd0b',1,'bitset.h']]],
+  ['bitset_5ffree_3',['bitset_free',['../bitset_8h.html#aeb7a593d109ebda7065f5e7eb80670b2',1,'bitset.h']]],
+  ['bitset_5fsize_4',['bitset_size',['../bitset_8h.html#a3e2e1542242e3d049e810bc1b8de670b',1,'bitset.h']]]
+];
--- makefile
+++ makefile
@@ -3,3 +3,7 @@ CFLAGS=-m32 -g -std=c11 -Wall -Wextra -pedantic -fsanitize=address
 
 test: bitset.c bitset.h
 	$(CC) $< -o $@ $(CFLAGS)
+
+CFLAGS+=-lm
+primes: eratosthenes.c
+	$(CC) $< -o $@ $(CFLAGS)
