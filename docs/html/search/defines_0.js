2a0fdb0dcbb6021125cc06247511b0a22cd6e4fe
Adam Zvara <xzvara01@stud.fit.vutbr.cz>
2021-03-03 22:24:18 +0100

    Removed documentation because it can be generated.

--- docs/html/search/defines_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['bitset_5falloc_7',['bitset_alloc',['../bitset_8h.html#a7c096bdf16b851a3e10b43c0acb8de60',1,'bitset.h']]],
-  ['bitset_5fcreate_8',['bitset_create',['../bitset_8h.html#aad4b5748beb7a8d9640862bdad74cd0b',1,'bitset.h']]],
-  ['bitset_5ffree_9',['bitset_free',['../bitset_8h.html#aeb7a593d109ebda7065f5e7eb80670b2',1,'bitset.h']]],
-  ['bitset_5fsize_10',['bitset_size',['../bitset_8h.html#a3e2e1542242e3d049e810bc1b8de670b',1,'bitset.h']]]
-];
--- docs/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['bitset_2eh_6',['bitset.h',['../bitset_8h.html',1,'']]]
-];
