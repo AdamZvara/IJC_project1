2b5a69420480b72fc7b3d3e1c98d8ae440b01474
Adam Zvara <xzvara01@stud.fit.vutbr.cz>
2021-03-10 09:29:39 +0100

    Message is now stored in dynamic array. Fixed missing \0 bit message.

--- steg-decode.c
+++ steg-decode.c
@@ -6,11 +6,25 @@
  * @details Compiled with GCC
  */
 
+#include <stdbool.h>
 #include "ppm.h"
 #include "eratosthenes.h"
 
 #define MAX_IMAGE_SIZE 8000*8000*3
 #define START 23
+
+void message_resize(char *name, unsigned int size)
+{
+    void *new_name = realloc(name, size);
+
+    if (new_name == NULL)
+    {
+        if (name != NULL) free(name);
+        error_exit("Alokacia spravy neprebehla spravne\n");
+    }
+
+    name = new_name;
+}
 
 int main(int argc, char *argv[])
 {
@@ -37,32 +51,66 @@ int main(int argc, char *argv[])
 
     Eratosthenes(bset);
 
-    unsigned int counter = 0;
-    int character = 0;
+    unsigned int bit_counter = 0;
+    unsigned int character = 0;
+    
+    unsigned int length = 100;
+    unsigned int character_count = 0;
+    char *message = malloc(length);
+    if (message == NULL)
+    {
+        ppm_free(image);
+        error_exit("Chyba pri alokacii spravy");
+    }
+    bool end_bit = false;
+    
     for (bitset_index_t i = START; i < bitset_size(bset); i++)
     {
         if (bitset_getbit(bset, i) == 0)
         {
             if ((image->data[i] & 1) == 1)
             {
-                character |= (1 << counter);
+                character |= (1 << bit_counter);
             }
             
-            if (counter == 7)
+            if (bit_counter == 7)
             {
-                putchar(character);
-                if (character == '\0') break;
+                if (character_count / length)
+                {
+                    length += 100;
+                    message_resize(message, length);
+                }
+
+                message[character_count++] = character;
+                
+                if (character == '\0') 
+                {
+                    end_bit = true;
+                    break;   
+                }
                 else 
                 {
-                    counter = 0;
+                    bit_counter = 0;
                     character = 0;
                     continue;
                 }
             }
-            counter++;
+            bit_counter++;
         }
     }
+
+    if (end_bit == false)
+    {
+        free(message);
+        ppm_free(image);
+        error_exit("Sprava nebola spravne ukoncena\n");
+    }
+    else
+    {
+        printf("%s", message);
+    }
     
+    free(message);
     bitset_free(bset);
     ppm_free(image);
     return 0;
